6c884cf80c7c60bf691265d50e64f09a07827f54
Luis Carlin <luiscarlin@gmail.com>
2021-12-19 15:26:08 -0500

    basic with pnpm

---
+++ with-pnpm/packages/ui/Button.tsx
@@ -0,0 +1,7 @@
+export const Button = () => {
+  return <button onClick={() => console.log('hi')}>Click me if you dare</button>;
+};
+
+export function MyImage() {
+  return <img src="https://picsum.photos/200" />;
+}
